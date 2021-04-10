2d482f94b2c6bc0e4e99677c931f586169c54175
sutymate <sutymate@fit.cvut.cz>
2021-04-10 12:48:19 +0200

    change hash id, fix minor unused import

--- preprocessing/token.py
+++ preprocessing/token.py
@@ -8,29 +8,29 @@ from vector.vector import make_docterm_vector
 
 def create_space(path):
     files = os.listdir(path)
-    save_dir = 'matrixes'
+    save_dir = 'matrices'
     cnt = 0
     docterm_list = []
     unique_doc_ids = []
     # preprocess
     for f in files:
         # testing break
-        if cnt > 10:
+        if cnt > 4:
             break
         cnt += 1
         document_file = open(path + f)
         document = document_file.read()
-        id = sha256(bytearray(document, encoding='utf8'))
-        if not id in unique_doc_ids:
-            unique_doc_ids.append(id)
+        doc_id = sha256(bytearray(document, encoding='utf8')).hexdigest()
+        if doc_id in unique_doc_ids:
+            print("double document", doc_id, path, f)
         else:
-            print("double document", id, path, f)
+            unique_doc_ids.append(doc_id)
         tokens = tokenize(document)
         clean_tokens = remove_stops(tokens)
         clean_words = stemmatize(clean_tokens)
         save_path = None  # save_dir + 'm_' + f
         # creates vector of terms with relative weight to this document
-        docterm = make_docterm_vector(clean_words, save_path, id=str(id))
+        docterm = make_docterm_vector(clean_words, save_path, doc_id=str(doc_id), article_filename=f)
         docterm_list.append(docterm)
     return docterm_list
 
@@ -60,7 +60,7 @@ def remove_stops(tokens, length_threshold=2):
     clean_tokens = []
     for word in tokens:
         if word not in stops:
-            if len(word) > 2:
+            if len(word) > length_threshold:
                 clean_tokens.append(word)
     return clean_tokens
 
