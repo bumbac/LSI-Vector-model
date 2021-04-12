56fa0d95677167fce7dd7db5d070c88e62f819eb
sutymate <sutymate@fit.cvut.cz>
2021-04-12 14:28:39 +0200

    fix top docs sorting, add documentation

--- LSI/retrieval.py
+++ LSI/retrieval.py
@@ -3,6 +3,13 @@ from numpy.linalg import norm
 
 
 def func(matrices_dict, query_vector):
+    """
+    Finds most similar articles from calculated matrices in matrices_dict
+    based on query_vector
+    :param matrices_dict: matrices from A = USV, D and S inverted
+    :param query_vector: dict{term: weight}
+    :return: descending list of tuples(doc_number, similarity)
+    """
     u = matrices_dict["U"]
     s = matrices_dict["S"]
     v = matrices_dict["V"]
@@ -10,19 +17,27 @@ def func(matrices_dict, query_vector):
     s_inv = matrices_dict["S_inv"]
     q_t = np.transpose(query_vector)
     q_new = np.linalg.multi_dot([q_t, u, s_inv])
-    doc_by_concept_list = []
     ranking_of_documents = {}
     for d_ in range(v.shape[1]):
+        print(d_)
         document = v[:][d_]
         ranking = similarity(q_new, document)
-        ranking_of_documents[d_] = ranking
-    sorted_documents = sorted(ranking_of_documents, key=ranking_of_documents.get)
-    top_k = 2
-
-    return sorted_documents[:top_k]
+        ranking_of_documents[d_] = ranking[0]
+    sorted_documents = sorted(ranking_of_documents, key=ranking_of_documents.get, reverse=True)
+    top_k = 3
+    sorted_doc_similarity = []
+    for doc_number in sorted_documents:
+        sorted_doc_similarity.append((doc_number, ranking_of_documents[doc_number]))
+    return sorted_doc_similarity[:top_k]
 
 
 def similarity(query, document):
+    """
+    Calculates cosine similarity of query to document.
+    :param query: dict{term: weight}
+    :param document: docterm vector from V matrice - transformed by SVD concepts
+    :return: float cosine similarity
+    """
     frac_top = np.dot(query, document)
     frac_bottom = (norm(query) * norm(document))
     return frac_top / frac_bottom
