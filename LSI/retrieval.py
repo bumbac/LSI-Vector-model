a004596d8bec65d6ca131e0eb67cf835295b546e
sutymate <sutymate@fit.cvut.cz>
2021-04-13 09:30:46 +0200

    change query from vec to similar documents

--- LSI/retrieval.py
+++ LSI/retrieval.py
@@ -2,47 +2,51 @@ import numpy as np
 from numpy.linalg import norm
 
 
-def func(matrices_dict, query_vector):
+def func(matrices_dict, doc_tuple):
     """
     Finds most similar articles from calculated matrices in matrices_dict
     based on query_vector
     :param matrices_dict: matrices from A = USV, D and S inverted
-    :param query_vector: dict{term: weight}
+    :param doc_tuple: tuple(doc_id, doc_filename)
     :return: descending list of tuples(doc_number, similarity)
     """
     u = matrices_dict["U"]
     s = matrices_dict["S"]
     v = matrices_dict["V"]
     d = matrices_dict["D"]
-    # v = np.transpose(v)
-    s_inv = matrices_dict["S_inv"]
-    q_t = np.transpose(query_vector)
-    q_new = np.linalg.multi_dot([q_t, u, s_inv])
+    doc_id = doc_tuple[0]
+    q_t = v[:, doc_id]
+    # dict(doc_id, similarity)
     ranking_of_documents = {}
+    # iterate all columns = documents in concept space (V)
     for d_ in range(v.shape[1]):
         document = v[:, d_]
-        ranking = similarity(q_new, document)
+        if d_ == doc_id:
+            # ranking should be == 1
+            ranking = similarity(q_t, document)
+            continue
+        ranking = similarity(q_t, document)
         ranking_of_documents[d_] = ranking
-    sorted_documents = sorted(ranking_of_documents, key=ranking_of_documents.get, reverse=True)
-    top_k = 4
+    # descending order of most relevant doc_ids
+    sorted_doc_ids = sorted(ranking_of_documents, key=ranking_of_documents.get, reverse=True)
+    top_k = 3
+    # tuples (doc_id, similarity)
     sorted_doc_similarity = []
-    for doc_number in sorted_documents:
-        sorted_doc_similarity.append((doc_number, ranking_of_documents[doc_number]))
-    return sorted_doc_similarity[:top_k]
+    for doc_id in sorted_doc_ids:
+        sorted_doc_similarity.append((doc_id, ranking_of_documents[doc_id]))
+    return sorted_doc_similarity[:top_k + 1]
 
 
 def similarity(query, document):
     """
     Calculates cosine similarity of query to document.
-    :param query: dict{term: weight}
-    :param document: docterm vector from V matrice - transformed by SVD concepts
+    :param query: document vector transformed to concept space q = S_inv {dot} U^T {dot} q
+    :param document: docterm vector from V matrix
     :return: float cosine similarity
     """
     frac_top = np.dot(query, document)
     frac_bottom = (norm(query) * norm(document))
-    # TODO
-    # WHAT DOES IT MEAN???
-    result = [0]
+    result = 0
     if frac_bottom != 0:
         result = frac_top / frac_bottom
-    return result[0]
+    return result
--- app/console.py
+++ app/console.py
@@ -3,76 +3,34 @@ import numpy as np
 from LSI import retrieval
 
 
-def read_input():
+def read_input(doc_filenames):
     """
-    Reads console input in specific form and returns parsed query
-    :return: dict{term: weight}
+    Reads console input for document name and finds it.
+    :return: tuple(doc_id, doc_filename)
     """
     correct_format = False
-    query = {}
     while not correct_format:
-        query = {}
         test = False
         line = ""
         if test:
-            line = "'poor 0.7 're 0.3"
+            line = 'article2.txt'
         else:
-            line = input("Write query with weights.\n"
-                         "For example: cat 0.4 food 0.5 cheap 0.1\n")
-        example_query = "cat 0.4 food 0.5 cheap 0.1"
-        tokens = line.split(" ")
-        # weight is missing, uneven number of parsed tokens
-        if len(tokens) % 2:
-            if len(tokens) == 1 and tokens[0] == "exit":
-                return {}
-            print("Incorrect format")
-            continue
-        weight = 0
-        token = ""
-        for i in range(len(tokens)):
-            # first name, then weight
-            # i % 2 == 1 for weights
-            if i % 2:
-                try:
-                    weight = float(tokens[i])
-                    query[token] = weight
-                except ValueError:
-                    print("Incorrect format.")
-                    break
-            # i % 2 == 0 for words
-            else:
-                token = tokens[i]
-                # too big token, probably not a word
-                if len(token) > 50:
-                    print("Incorrect format.")
-                    break
-        correct_format = True
-    return query
+            line = input("Write filename of article to find similar."
+                         "For example: article2.txt\n")
+        # item = (doc_id, doc_filename)
+        for item in doc_filenames.items():
+            if line == item[1]:
+                return item
 
 
 def start(matrices_dict):
     """
-    CLI interface for fetching similar documents to query
+    CLI interface for fetching similar documents.
     :param matrices_dict: dict of calculated matrices U, S, V, D, S_inv
     :return: descending list of tuples(doc_number, similarity)
     """
-    terms = matrices_dict["Terms"]
+    doc_filenames = matrices_dict['doc_filenames']
     while True:
-        tokens = read_input()
-        if len(tokens) == 0:
-            return {}
-        print(tokens)
-        query_vector = np.zeros((len(terms), 1), dtype=float)
-        # assign weight to all terms
-        for term in tokens.keys():
-            term_id = bisect.bisect_left(terms, term)
-            # term in query is not present in language space
-            if term_id == len(terms):
-                continue
-            # term is present in the language space, update query vector
-            if terms[term_id] == term:
-                query_vector[term_id, 0] = tokens[term]
-            # term in query is not present in language space
-            if terms[term_id] != term:
-                continue
-        return retrieval.func(matrices_dict, query_vector)
+        article_tuple = read_input(doc_filenames)
+        return retrieval.func(matrices_dict, article_tuple)
+
