d9110f02d966d65e719aa28c5392437543d9bc70
sutymate <sutymate@fit.cvut.cz>
2021-05-07 08:43:58 +0200

    delete redundant articles, create test struct

---
+++ experiments/main.py
@@ -0,0 +1,22 @@
+
+def speed_test():
+    n = 1000
+    k = [10, 20, 30, 40, 50]
+    # EXP  linearne...
+
+    pass
+
+
+def k_test():
+    # k, k+1 zvysuju rozpty
+    pass
+
+
+def similarity_test():
+    pass
+
+
+if __name__ == '__main__':
+    speed_test()
+    k_test()
+    similarity_test()
