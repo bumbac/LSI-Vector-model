7923b7126051a8c25f98dfd9da5457e9367d85c2
sutymate <sutymate@fit.cvut.cz>
2021-05-07 06:02:08 +0200

    add extensive crawling and file check for english

--- crawler.py
+++ crawler.py
@@ -1,45 +1,64 @@
+import os
+import shutil
 import newspaper
 from newspaper import Article
+from preprocessing.language import is_eng
 
 
+def crawl():
+    #A new article from TOI
+    filepath = 'articles/matej2/'
+    url = "https://fashionmagazine.com/style"
+    #For different language newspaper refer above table
+    toi_articl = newspaper.build(url, memoize_articles=False, language="en") # en for English
+    category = 'fashion4_'
+    i = 0
 
-
-#A new article from TOI
-filepath = 'app/app/matej/2/'
-page_name = "garden"
-i = 0
-i_max = 200
-preventive = 9999999999999999999999999999999999
-for page_n in range(2, 20):
-# for page_n in range(0, 1):
-    url = "https://veggiegardeningtips.com/all-articles/page/" + str(page_n)
-    toi_articl = newspaper.build(url, memoize_articles=False, language="en")  # en for English
-    k = 0
     for article in toi_articl.articles:
-        k += 1
+        if i > 200:
+            break
         article.download()
         article.parse()
         # print(filename)
-        file = open(filepath + page_name + str(i)+".txt", "w")
+        file = open(filepath + category + str(i)+".txt", "w")
         file.write("Title: "+article.title)
         file.write("\n\n")
 
         text = article.text
+        print(len(text), article.title)
         text = text.replace("\n\n", " ")
         file.write("Text: "+text)
         file.close()
         i = i + 1
-        print(i, '/', len(toi_articl.articles), article.title)
-        if k > preventive or i > i_max:
-            break
 
 
 
-# # Downloading the HTML for the article
-# url = 'https://www.nytimes.com'
-# article = Article(url)
-# article.download()
-# article.parse()
-# with open('fox13no.txt', 'w') as fileout:
-#    fileout.write(article.title+"\n")
-#    fileout.write(article.text)
+    # # Downloading the HTML for the article
+    # url = 'https://www.nytimes.com'
+    # article = Article(url)
+    # article.download()
+    # article.parse()
+    # with open('fox13no.txt', 'w') as fileout:
+    #    fileout.write(article.title+"\n")
+    #    fileout.write(article.text)
+
+def preprocess():
+    articles_folder = 'matej2/'
+    error_folder = 'matej2/error/'
+    path = 'articles/' + articles_folder
+    error_path = 'articles/' + error_folder
+    for filename in os.listdir(path):
+        if filename.endswith(".txt"):
+            f = open(path + filename, 'r')
+            article_text = f.read()
+            f.close()
+            if is_eng(article_text):
+                continue
+            else:
+                shutil.move(path + filename, error_path + filename)
+                print(filename, 'moved to', error_folder)
+
+
+if __name__ == '__main__':
+    preprocess()
+    #crawl()
