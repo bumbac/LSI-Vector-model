5dcc7cf6169e781b40479f20fe61e25e8bd2a147
sutymate <sutymate@fit.cvut.cz>
2021-05-15 12:31:55 +0200

    change structure for experiments packages

--- lsi-data/experiments/main.py
+++ lsi-data/experiments/main.py
@@ -2,9 +2,8 @@ import os
 import time
 import pandas as pd
 import pickle
-from run import create_index
 import matplotlib.pylab as plt
-from app.lsi import models as lsi
+from lsi import models as lsi
 
 
 def speed_test():
@@ -34,12 +33,12 @@ def speed_test():
 
 def k_test():
     """
-       View which displays selected article with similarity of other articles.
-       :param request: django request
-       :param article_id: selected article id
-       :return: article: object that contains title and content.
+       Runs experiment on concept space. Selects random files from directory and for various k plots the similiarty
+       of three most similar articles. For various k, plots histogram of most frequent articles on position 1, 2, 3.
        """
+    # select directory with analyzed articles. Chooses one randomly, all need to be processed by LSI before
     files = os.listdir('../articles/tmp4')
+    # set how many files you want to analyze, 1-3 recommended
     for i in range(3):
         article_filename = files[i]
 
@@ -47,11 +46,14 @@ def k_test():
         matrix_f = open(matrix_path + 'file.dat', 'rb')
         matrix_data = matrix_f.read()
         matrices_dict = pickle.loads(matrix_data)
+        # how many articles in top positions should be analyzed
         k_articles = 3 + 1
         similarity_top5 = [[] for i in range(k_articles)]
         order_articles = [[] for i in range(k_articles)]
+        # set range for approximation.
         max_approx = 490
         tested_values = []
+        # see stepping for faster calculations
         for i in range(1, 30, 5):
             tested_values.append(i)
         for i in range(30, max_approx, 10):
@@ -61,6 +63,7 @@ def k_test():
             article_tuple = lsi.find_file(doc_filenames, article_filename)
             top = lsi.func(matrices_dict, article_tuple, approx)
             cnt = 0
+            # analyzes 0 to k_articles
             for doc_sim_tuple in top[:k_articles]:
                 similar_document_id = doc_sim_tuple[0]
                 similar_document_name = str(matrices_dict['doc_filenames'][similar_document_id]).replace('.txt', '')
@@ -69,6 +72,7 @@ def k_test():
                 order_articles[cnt].append(similar_document_name)
                 cnt += 1
         article_filename = article_filename.replace('.txt', '')
+        # histogram with frequencies of articles in top positons
         for i in range(1, k_articles):
             ax = pd.Series(order_articles[i]).value_counts().head(5).plot(kind='bar', title=str(i) + '. most similar '
                                                                                                'articles '
@@ -76,9 +80,9 @@ def k_test():
                                                                                + article_filename)
             plt.xticks(rotation=0)
             fig = ax.get_figure()
-            fig.savefig('out/4/hist_' + article_filename + str(i) + '.png')
+            fig.savefig('out/5/hist_' + article_filename + str(i) + '.png')
             fig.show()
-
+        # similarities in top positions
         for i in range(1, k_articles):
             plt.plot(tested_values, similarity_top5[i], label=str(i) + '. most similar')
             plt.title("Articles with best match to " + article_filename)
@@ -87,15 +91,10 @@ def k_test():
             plt.legend()
 
 
-        plt.savefig('out/4/k_' + article_filename + '.png')
+        plt.savefig('out/5/k_' + article_filename + '.png')
         plt.show()
-
-
-def similarity_test():
-    pass
 
 
 if __name__ == '__main__':
     # speed_test()
     k_test()
-    # similarity_test()
